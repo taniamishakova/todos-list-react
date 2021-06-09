74807518c29dc1341a932ae85315bbb92e864d56
Tania Mishakova <tmishakova123@gmail.com>
2021-06-09 13:14:31 +0200

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -38,6 +38,17 @@ function App() {
     })));
   };
 
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ]);
+  };
+
   return (
     <Container>
       <Header
@@ -46,7 +57,7 @@ function App() {
 
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form />}
+        body={<Form addNewTask={addNewTask} />}
       />
 
       <Section
